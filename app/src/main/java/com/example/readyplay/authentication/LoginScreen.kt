23125a42413d510d509291dffef2617c2619e84c
Fredoski26 <41081452+Fredoski26@users.noreply.github.com>
2024-10-04 11:30:16 +0100

    Changed LoginScreen textColor for email and password

--- app/src/main/java/com/example/readyplay/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/readyplay/authentication/LoginScreen.kt
@@ -24,6 +24,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -36,6 +38,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -103,6 +106,9 @@ fun LoginScreen(navController: NavHostController) {
 
         OutlinedTextField(
             value = email,
+            textStyle = TextStyle(
+                color = Color.Black
+            ),
             onValueChange = { email = it
                 showError = false },
             label = { Text("name@example.com") },
@@ -134,6 +140,9 @@ fun LoginScreen(navController: NavHostController) {
 
         OutlinedTextField(
             value = password,
+            textStyle = TextStyle(
+                color = Color.Black
+            ),
             onValueChange = { password = it
                 showErrorPass = false },
             label = { Text("Password") },
